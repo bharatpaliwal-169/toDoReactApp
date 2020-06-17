8f6511c399bcbe4c51168cfd4dbf9fc67158b655
Bharat <18uec169@lnmiit.ac.in>
2020-06-17 22:07:01 +0530

    Updates

---
+++ precache-manifest.922a9ca4db7758c412ff103f6200311c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd1efaf2ffc530d88c51147427411be6",
+    "url": "/toDoReactApp/index.html"
+  },
+  {
+    "revision": "85ddcf373b2a435775f7",
+    "url": "/toDoReactApp/static/css/main.5c25dc30.chunk.css"
+  },
+  {
+    "revision": "5392aea3c96d33bc02e3",
+    "url": "/toDoReactApp/static/js/2.d12b7167.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/toDoReactApp/static/js/2.d12b7167.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85ddcf373b2a435775f7",
+    "url": "/toDoReactApp/static/js/main.4b44bbb7.chunk.js"
+  },
+  {
+    "revision": "2d9da6c6c13af61dba53",
+    "url": "/toDoReactApp/static/js/runtime-main.d8dc1969.js"
+  }
+]);
